06e79730dcecd4be047bd827714e0e4128801a25
Jj <jjdelc@gmail.com>
2011-11-27 01:50:38 -0500

    Options to sepia backend

--- README.md
+++ README.md
@@ -3,13 +3,23 @@
 Sorl-thumbnail engine to convert images to sepia.
 Currently only supports PIL engine
 
-## Instalation
+## Instalation and usage
 
 Add 'sorl_sepia.engine.Engine' as your THUMBNAIL_ENGINE
 
-To change the sepia tone, set the SORL_DEFAULTSEPIA_TONE settings which is an (int, int, int) tuple.
+If you want all your thumbnails to be sepia, set the SORL_SEPIA_ALWAYS settings to True
+SORL_SEPIA_ALWAYS = True # Default value is False
 
-SORL_DEFAULTSEPIA_TONE = (255, 240, 192) # Default value
+To change the sepia tone, set the SORL_DEFAULT_SEPIA_TONE settings which is an (int, int, int) tuple.
+
+SORL_DEFAULT_SEPIA_TONE = (255, 240, 192) # Default value
+
+Alternatively, if you want to enable sepia and tone per image, use the 'sepia' and 'sepia_tone' thumbnail templatetag options:
+
+{% thumbnail my_pic.image_file "100x100" sepia=True sepia_tone="255,240,193" as thumb %}
+
+* `sepia` set to True to convert such picture to sepia
+* `sepia_tone` is a comma separated string indicating RGB values
 
 # TODO
 
--- sorl_sepia/engine.py
+++ sorl_sepia/engine.py
@@ -7,26 +7,31 @@ from django.conf import settings
 from sorl.thumbnail.engines.base import EngineBase
 from sorl.thumbnail.engines.pil_engine import Engine as PILEngine
 
+
 # Set this tone on settings to turn to sepia or any other tone
-SEPIA_DEFAULT_TONE = getattr(settings, 'SORL_DEFAULTSEPIA_TONE', 
+SEPIA_DEFAULT_TONE = getattr(settings, 'SORL_DEFAULT_SEPIA_TONE', 
     (255, 240, 192))
+SEPIA_ALWAYS = getattr(settings, 'SORL_SEPIA_ALWAYS', False)
+
+
+def _make_linear_ramp(white):
+    """ Normalizes a RGB tuple to 0-1 values """ 
+    ramp = []
+    r, g, b = white
+    for i in range(255):
+        ramp.extend((r*i/255, g*i/255, b*i/255))
+
+    return ramp
+
 
 # Based on http://effbot.org/zone/pil-sepia.htm
 class SepiaPILEngineMixin(object):
 
-    @staticmethod
-    def _make_linear_ramp(white):
-        """ Normalizes a RGB tuple to 0-1 values """ 
-        ramp = []
-        r, g, b = white
-        for i in range(255):
-            ramp.extend((r*i/255, g*i/255, b*i/255))
+    def sepia(self, image, tone=None):
+        if tone is None:
+            tone = SEPIA_DEFAULT_TONE
 
-        return ramp
-    
-    def sepia(self, image):
-
-        sepia = self._make_linear_ramp(SEPIA_DEFAULT_TONE)
+        sepia = _make_linear_ramp(tone)
 
         original_mode = image.mode
         # Make grayscale
@@ -37,10 +42,22 @@ class SepiaPILEngineMixin(object):
         image = image.convert(original_mode)
 
         return image
-        
+    
 
 class Engine(SepiaPILEngineMixin, PILEngine):
     def create(self, image, geometry, options):
         image = super(Engine, self).create(image, geometry, options)
 
-        return self.sepia(image)
+        tone = None
+        if 'sepia_tone' in options:
+            tone = [int(val.strip(), 10) 
+                for val in options['sepia_tone'].split(',')]
+
+        if SEPIA_ALWAYS:
+            return self.sepia(image, tone)
+        
+        if 'sepia' in options:
+
+            return self.sepia(image, tone)
+
+        return image
